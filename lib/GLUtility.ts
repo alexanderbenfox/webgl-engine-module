b8c6285af3fdaeae8ede34d5a14c3b0fabb3a6ca
Alex Fox <alexanderbenfox@gmail.com>
2019-01-01 18:27:58 -0500

    Added depth test

--- lib/GLUtility.ts
+++ lib/GLUtility.ts
@@ -44,7 +44,8 @@ export module GLUtility{
 		gl.clearColor(0.0,0.0,0.0,1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		gl.depthFunc(gl.LEQUAL);
-		gl.disable(gl.DEPTH_TEST);
+		gl.enable(gl.DEPTH_TEST);
+		//gl.disable(gl.DEPTH_TEST);
 		//gl.disable(gl.CULL_FACE);
 		gl.enable(gl.CULL_FACE);
 		gl.enable(gl.BLEND);
